1198775d676fe74b4c4d4b525987a2921da1e30c
macosgrove <maryanne@covidence.org>
2025-01-19 14:12:48 +1100

    Hello python

---
+++ main.py
@@ -0,0 +1,31 @@
+# Import the 'unittest' module for writing unit tests.
+import unittest
+
+def count_nineteen_five(list):
+    count = {19:0, 5:0}
+    for i in list:
+        if i == 19: count[19] += 1
+        if i == 5: count[5] += 1
+    return count
+
+def check_nineteen_five(list):
+    count = count_nineteen_five(list)
+    return count[19] == 2 and count[5] >= 3
+
+class PrimeNumberTestCase(unittest.TestCase):
+    def test_count_nineteen_five(self):
+        list = [19, 5, 5, 3, 19, 21, 19]
+        self.assertTrue(count_nineteen_five(list) == {19: 3, 5: 2})
+        self.assertTrue(count_nineteen_five([]) == {19: 0, 5: 0})
+
+    # Define a test method 'test_non_prime_numbers' to test non-prime numbers.
+    def test_check_nineteen_five(self):
+        self.assertFalse(check_nineteen_five([]))
+        self.assertTrue(check_nineteen_five([19, 19, 15, 5, 3, 5, 5, 2]))
+        self.assertFalse(check_nineteen_five([19, 15, 15, 5, 3, 3, 5, 2]))
+        self.assertTrue(check_nineteen_five([19, 19, 5, 5, 5, 5, 5]))
+
+# Check if the script is run as the main program.
+if __name__ == '__main__':
+    # Run the test cases using 'unittest.main()'.
+    unittest.main()
